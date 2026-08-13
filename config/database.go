3abbbb8730609f4b9230be25bc4c56bbaac5f4b4
Ibrahim Mohamed Bush <ibrahimbush2006@gmail.com>
2026-08-13 21:32:40 +0300

    feat: create database connection function and create tables with AutoMigrate

---
+++ config/database.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"Wallet-App/model"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Init_database(connection_data string) (*gorm.DB, error) {
+
+	//first get connection to the database.
+	db, err := gorm.Open(postgres.Open(connection_data), &gorm.Config{TranslateError: true})
+	//check if something went wrong.
+	if err != nil {
+		return nil, err
+	}
+	//create a table with AutoMigrate.
+	err = db.AutoMigrate(&model.User{}, &model.Wallet{}, &model.Transaction{})
+	if err != nil {
+		return nil, err
+	}
+	//if the connection and autoMigration succeeded, return the database object.
+	return db, nil
+}
